d9416203658309f602ec6a5ded9bbdc54f4b32cb
Ivan Pykalo/SoftServe <ipyka@softserveinc.com>
2020-04-17 17:23:18 +0300

    added auth mmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AmplifyAngularModule, AmplifyService, AmplifyModules } from 'aws-amplify-angular';
+import { Auth } from '@aws-amplify/auth';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AmplifyAngularModule
+    ],
+    providers: [
+        {
+            provide: AmplifyService,
+            useFactory: () => {
+                return AmplifyModules({
+                    Auth
+                });
+            }
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
